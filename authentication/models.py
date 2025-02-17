de8563033201760b44cb4ba08caed58243eb06b3
RedSSD <salatcaesarrr1@gmail.com>
2025-02-18 00:08:17 +0100

    Added authentication app, basic user model and manager, endpoints for JWT obtain and refresh

---
+++ authentication/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    nickname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'nickname',
+    ]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
