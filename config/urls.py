cdc0f6cd2ebea71ce951d78dcf94ee111c14a18f
RedSSD <salatcaesarrr1@gmail.com>
2025-02-18 00:32:24 +0100

    Added swagger auto documentation

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Realm of Spells API",
+        default_version='v1',
+        url="http://localhost:8000/api/v1/",
+        description="API documentation for Realm of Spells",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("authentication/", include("authentication.urls")),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("api/", include("authentication.urls")),
 ]
